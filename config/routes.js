9495c8775c0487dbe8a09e02922268cea637ee75
jeffj <jenkinsjeffrey@gmail.com>
2014-08-15 18:16:07 -0700

    wowo can i really explain this one.  I'm trying to merge tow apps.

---
+++ config/routes.js
@@ -0,0 +1,120 @@
+/*!
+ * Module dependencies.
+ */
+
+var async = require('async')
+
+/**
+ * Controllers
+ */
+
+var users = require('../app/controllers/users')
+  , articles = require('../app/controllers/articles')
+  , auth = require('./middlewares/authorization')
+  , crudUtils = require('../utils/crudUtils');
+    mongoose = require('mongoose')
+  , Todo = mongoose.model('Todo');
+
+/**
+ * Route middlewares
+ */
+
+var articleAuth = [auth.requiresLogin, auth.article.hasAuthorization]
+var commentAuth = [auth.requiresLogin, auth.comment.hasAuthorization]
+
+/**
+ * Expose routes
+ */
+
+module.exports = function (app, passport) {
+
+  // user routes
+  app.get('/login', users.login)
+  app.get('/signup', users.signup)
+  app.get('/logout', users.logout)
+  app.post('/users', users.create)
+  app.post('/users/session',
+    passport.authenticate('local', {
+      failureRedirect: '/login',
+      failureFlash: 'Invalid email or password.'
+    }), users.session)
+  app.get('/users/:userId', users.show)
+  app.get('/auth/facebook',
+    passport.authenticate('facebook', {
+      scope: [ 'email', 'user_about_me'],
+      failureRedirect: '/login'
+    }), users.signin)
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      failureRedirect: '/login'
+    }), users.authCallback)
+  app.get('/auth/github',
+    passport.authenticate('github', {
+      failureRedirect: '/login'
+    }), users.signin)
+  app.get('/auth/github/callback',
+    passport.authenticate('github', {
+      failureRedirect: '/login'
+    }), users.authCallback)
+  app.get('/auth/twitter',
+    passport.authenticate('twitter', {
+      failureRedirect: '/login'
+    }), users.signin)
+  app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', {
+      failureRedirect: '/login'
+    }), users.authCallback)
+  app.get('/auth/google',
+    passport.authenticate('google', {
+      failureRedirect: '/login',
+      scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+      ]
+    }), users.signin)
+  app.get('/auth/google/callback',
+    passport.authenticate('google', {
+      failureRedirect: '/login'
+    }), users.authCallback)
+  app.get('/auth/linkedin',
+    passport.authenticate('linkedin', {
+      failureRedirect: '/login',
+      scope: [
+        'r_emailaddress'
+      ]
+    }), users.signin)
+  app.get('/auth/linkedin/callback',
+    passport.authenticate('linkedin', {
+      failureRedirect: '/login'
+    }), users.authCallback)
+
+  app.param('userId', users.user)
+
+  // article routes
+  app.param('id', articles.load)
+  app.get('/articles', articles.index)
+  app.get('/articles/new', auth.requiresLogin, articles.new)
+  app.post('/articles', auth.requiresLogin, articles.create)
+  app.get('/articles/:id', articles.show)
+  app.get('/articles/:id/edit', articleAuth, articles.edit)
+  app.put('/articles/:id', articleAuth, articles.update)
+  app.del('/articles/:id', articleAuth, articles.destroy)
+
+  // home route
+  app.get('/', articles.index)
+
+  // comment routes
+  var comments = require('../app/controllers/comments')
+  app.param('commentId', comments.load)
+  app.post('/articles/:id/comments', auth.requiresLogin, comments.create)
+  app.get('/articles/:id/comments', auth.requiresLogin, comments.create)
+  app.del('/articles/:id/comments/:commentId', commentAuth, comments.destroy)
+
+  // tag routes
+  var tags = require('../app/controllers/tags')
+  app.get('/tags/:tag', tags.index)
+
+  crudUtils.initRoutesForModel({ 'app': app, 'model': Todo });
+
+
+}
