22b7623d56b9e79ee14d9d6fe1e6b629b6242abd
jeffj <jenkinsjeffrey@gmail.com>
2014-11-17 17:06:16 -0800

    removing crud app.

--- crud/models/index.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-
-  TodoSchema = new mongoose.Schema({
-    title: { 'type': String, 'default': 'empty todo...' },
-    order: { 'type': Number },
-    done: { 'type': Boolean, 'default': false }
-  });
-
-module.exports = mongoose.model('Todo', TodoSchema);
